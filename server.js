fb5de71fde02e93ef5c5c5b9310c34d6dee7b308
Sparsh <gsparsh17@gmail.com>
2024-09-28 02:29:41 +0530

    comit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const JWT_SECRET = "jwt";
 
